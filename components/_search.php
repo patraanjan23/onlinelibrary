ca8bf4fbd0828f4e4b60ed7adf7ab30bb28e0852
Dipanjan Patra <patraanjan23@gmail.com>
2021-07-21 17:21:24 +0530

    radio btn states are preserved now

--- components/_search.php
+++ components/_search.php
@@ -1,17 +1,24 @@
+<?php
+if (!isset($GLOBALS['searchBy'])) {
+    $GLOBALS['searchBy'] = 'title';
+} else {
+    $searchBy = $GLOBALS['searchBy'];
+}
+?>
 <div class="search">
     <form action="search.php" method="get">
         <div class="center ">
             <div class="option">
                 <label>
-                    <input type="radio" name="searchby" id="title" value="title" checked>
+                    <input type="radio" name="searchby" id="title" value="title" <?php if (isset($searchBy) && $searchBy === 'title') echo 'checked'; ?>>
                     Title
                 </label>
                 <label>
-                    <input type="radio" name="searchby" id="author" value="author">
+                    <input type="radio" name="searchby" id="author" value="author" <?php if (isset($searchBy) && $searchBy === 'author') echo 'checked'; ?>>
                     Author
                 </label>
                 <label>
-                    <input type="radio" name="searchby" id="isbn" value="isbn">
+                    <input type="radio" name="searchby" id="isbn" value="isbn" <?php if (isset($searchBy) && $searchBy === 'isbn') echo 'checked'; ?>>
                     ISBN
                 </label>
             </div>
