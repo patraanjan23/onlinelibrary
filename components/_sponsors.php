b6ddc6083d4aa793a44a55878abaf253745407d1
Dipanjan Patra <patraanjan23@gmail.com>
2021-07-21 16:44:43 +0530

    updated sponsor images

--- components/_sponsors.php
+++ components/_sponsors.php
@@ -1,6 +1,6 @@
 <h4>Our Sponsors</h4>
 <ul>
-    <li><img src="assets/l1.jpg" height="25px" width="25px"></img>Tata Group</li>
-    <li><img src="assets/l2.jpg" height="25px" width="25px"></img>UNISCO</li>
+    <li><img src="assets/l1.png" height="25px" width="25px"></img>Tata Group</li>
+    <li><img src="assets/l2.png" height="25px" width="25px"></img>UNISCO</li>
     <li><img src="assets/l3.png" height="25px" width="25px"></img>Government of India</li>
 </ul>
