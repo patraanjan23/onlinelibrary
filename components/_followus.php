b6ec764c2fbd5af3c68fe6e1fd9a8369f8f3d467
Dipanjan Patra <patraanjan23@gmail.com>
2021-08-29 17:36:02 +0530

    replaced links with dummy links

--- components/_followus.php
+++ components/_followus.php
@@ -2,22 +2,22 @@
 <h4>Follow Us</h4>
 <ul>
     <li>
-        <a href="https://www.facebook.com/cambridgeuniversitylibrary" class="fa fa-facebook">
+        <a href="https://www.facebook.com/" class="fa fa-facebook">
         </a>
         Facebook
     </li>
     <li>
-        <a href="https://www.instagram.com/cambridgeuniversitylibrary/" class="fa fa-instagram">
+        <a href="https://www.instagram.com/" class="fa fa-instagram">
         </a>
         Instagram
     </li>
     <li>
-        <a href="https://twitter.com/theUL?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" class="fa fa-twitter">
+        <a href="https://twitter.com/" class="fa fa-twitter">
         </a>
         Twitter
     </li>
     <li>
-        <a href="https://www.youtube.com/channel/UCyIkmkqcZzRCy5bkeV6ojdQ" class="fa fa-youtube">
+        <a href="https://www.youtube.com/" class="fa fa-youtube">
         </a>
         Youtube
     </li>
