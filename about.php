b77e1f74f6165f0f7006b5635ffacf02da1b591b
Dipanjan Patra <patraanjan23@gmail.com>
2021-08-29 17:19:39 +0530

    added carousel in about page

--- about.php
+++ about.php
@@ -6,6 +6,8 @@
 <head>
     <title><?php require('components/_title.php'); ?></title>
     <?php require('includes/styles.php'); ?>
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
 </head>
 
 <body>
@@ -15,6 +17,18 @@
         <div class="container about-page padded rounded4 width80">
             <h2>About The Library</h2>
             <p>Over the course of six centuries the Royal Library’s collections have grown from a few dozen volumes on a handful of subjects into an extraordinary accumulation of several million books, maps, manuscripts and journals, augmented by an ever-increasing range of electronic resources. They cover every conceivable aspect of human endeavour, across three thousand years and in over two thousand languages. From its beginnings as an asset for a tiny community of theologians and canon lawyers in the medieval university, the Library’s mission has expanded to serve the international scholarly community and now, through its digitisation projects, to reach new audiences across the world.</p>
+            <div class="swiper smaller">
+                <!-- Additional required wrapper -->
+                <div class="swiper-wrapper">
+                    <!-- Slides -->
+                    <img src="assets/photos/A.jpg" alt="1" class="swiper-slide">
+                    <img src="assets/photos/B.jpg" alt="1" class="swiper-slide">
+                    <img src="assets/photos/C.jpg" alt="1" class="swiper-slide">
+                    <img src="assets/photos/D.jpg" alt="1" class="swiper-slide">
+                    <img src="assets/photos/E.jpg" alt="1" class="swiper-slide">
+                    <img src="assets/photos/F.jpg" alt="1" class="swiper-slide">
+                </div>
+            </div>
             <p>The Library keeps evolving. In recent years we have been given the magnificent Montaigne Library of Gilbert de Botton, the literary archive of Dame Margaret Drabble, and the Chadwyck-Healey Liberation Collection, the definitive collection of French books published following the German retreat from French soil on 25 August 1944, donated by Sir Charles Chadwyck-Healey. Our major purchases have included the archive of the war poet Siegfried Sassoon and the important manuscript of St Luke’s Gospel known as the ‘Codex Zacynthius’, both supported with grants from the National Heritage Memorial Fund and the Friends of the National Libraries. A ground-breaking collaboration with the Bodleian Libraries also saw the Library jointly acquire the Lewis-Gibson Collection of Hebrew and Arabic manuscripts from the Cairo Genizah, collected by the twin Scottish sisters Agnes Lewis and Margaret Gibson.</p>
             <p>So while we conserve this unique cultural heritage for the future, we are simultaneously finding new ways to share it with the present generation by building a digital library. Anyone with an internet connection and a desire for knowledge can view letters written by Moses Maimonides, Newton’s autograph propositions on elliptic motion, or sketchbooks from Darwin’s voyage of the beagle. Through the digital library, communities of readers around the globe can help create a richer understanding of the material held in our care.</p>
             <p>Great collections are brought to life by great people – students and scholars, and visitors to the Library past, present and future.</p>
@@ -22,6 +36,7 @@
         </div>
     </div>
     <?php require('components/_footer.php'); ?>
+    <script src="scripts/bookcarousel.js"></script>
 </body>
 
 </html>
